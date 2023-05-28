dc752fce481106c413cdf9d58749a6c37d23836f
abi-widiarta <abiwidiarta03@gmail.com>
2023-05-28 18:57:28 +0700

    Code refactor

--- web_server.py
+++ web_server.py
@@ -20,30 +20,31 @@ while True:
             filename = b'index.html'
 
         with open(filename, 'rb') as file:
-            content = file.read()
+            outputdata = file.read()
 
-        response_header = "HTTP/1.1 200 OK\r\n".encode()
+        response_header = "HTTP/1.1 200 OK\r\n"
+
         if filename.endswith(b".html"):
-          response_header += b"Content-Type: text/html\r\n"
+          content_type = "Content-Type: text/html\r\n"
         elif filename.endswith(b".css"):
-          response_header += b"Content-Type: text/css\r\n"
+          content_type = "Content-Type: text/css\r\n"
         elif filename.endswith(b".jpg") or filename.endswith(b".jpeg"):
-          response_header += b"Content-Type: image/jpeg\r\n"
+          content_type = "Content-Type: image/jpeg\r\n"
         elif filename.endswith(b".svg"):
-          response_header += b"Content-Type: image/svg+xml\r\n"
+          content_type = "Content-Type: image/svg+xml\r\n"
         else:
-          response_header += b"Content-Type: text/plain\r\n"
+          content_type = "Content-Type: text/plain\r\n"
 
-        response_header += "\r\n".encode()
-        response = response_header + content
     except IOError:
-        response_header = "HTTP/1.1 404 Not Found\r\n".encode()
-        response_header += "Content-Type: text/html\r\n".encode()
-        response_header += "\r\n".encode()
-        response_body = "<html><head></head><body><h1>404 Not Found</h1></body></html>".encode()
-        response = response_header + response_body
+        response_header = "HTTP/1.1 404 Not Found\r\n"
+        content_type = "Content-Type: text/html\r\n"
+        outputdata = "<html><head></head><body><h1>404 Not Found</h1></body></html>".encode()
 
-    connectionSocket.sendall(response)
+    connectionSocket.send(response_header.encode())
+    connectionSocket.send(content_type.encode())
+    connectionSocket.send("\r\n".encode())
+
+    connectionSocket.send(outputdata)
     connectionSocket.close()
 
 serverSocket.close()
